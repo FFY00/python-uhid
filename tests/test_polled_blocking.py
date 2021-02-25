999189e4b8acccd6b846499825097df09770da72
Filipe Laíns <lains@riseup.net>
2021-02-25 22:54:55 +0000

    tests: add test for output event on polling blocking
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_polled_blocking.py
+++ tests/test_polled_blocking.py
@@ -1,3 +1,7 @@
+import os
+import threading
+import time
+
 import pytest
 
 import uhid
@@ -19,3 +23,41 @@ def test_create(basic_mouse_rdesc):
     assert device.country == 0
 
     device.destroy()
+
+
+@pytest.fixture()
+def vendor_device(vendor_rdesc):
+    device = uhid.UHIDDevice(0x4321, 0x1234, 'Dummy Mouse', vendor_rdesc, backend=uhid.PolledBlockingUHID)
+    device.wait_for_start()
+
+    stop_dispatch = threading.Event()
+    dispatch_thread = threading.Thread(target=device.dispatch, args=(stop_dispatch,))
+    dispatch_thread.start()
+
+    try:
+        yield device
+    finally:
+        stop_dispatch.set()
+        device.destroy()
+
+
+@pytest.fixture()
+def vendor_hidraw(vendor_device):
+    return find_hidraw(vendor_device)
+
+
+def test_output(vendor_device, vendor_hidraw):
+    data = [0, 1, 2, 3, 4, 5, 6, 7]
+    received = []
+
+    def receive_output(data, report_type):
+        received.append(data)
+
+    vendor_device.receive_output = receive_output
+
+    os.write(vendor_hidraw.fd, bytes(data))
+    os.write(vendor_hidraw.fd, bytes(list(reversed(data))))
+
+    time.sleep(0.1)  # give some time for the events to be dispatched
+
+    assert received == [data, list(reversed(data))]
