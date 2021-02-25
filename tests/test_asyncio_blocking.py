02e9509037fe82a1605e869951aaa1feee6e3540
Filipe Laíns <lains@riseup.net>
2021-02-25 16:54:15 +0000

    uhid: handle UHID output events
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- tests/test_asyncio_blocking.py
+++ tests/test_asyncio_blocking.py
@@ -1,3 +1,6 @@
+import asyncio
+import os
+
 import pytest
 
 import uhid
@@ -5,8 +8,8 @@ import uhid
 from . import find_hidraw
 
 
-@pytest.mark.asyncio
 @pytest.mark.timeout(1)
+@pytest.mark.asyncio
 async def test_create(basic_mouse_rdesc):
     device = uhid.UHIDDevice(0x1234, 0x4321, 'Dummy Mouse', basic_mouse_rdesc, backend=uhid.AsyncioBlockingUHID)
 
@@ -20,3 +23,35 @@ async def test_create(basic_mouse_rdesc):
     assert hidraw.report_descriptor == device.report_descriptor == basic_mouse_rdesc
     assert device.version == 0
     assert device.country == 0
+
+
+@pytest.fixture()
+@pytest.mark.asyncio
+async def vendor_device(vendor_rdesc):
+    device = uhid.UHIDDevice(0x4321, 0x1234, 'Dummy Mouse', vendor_rdesc, backend=uhid.AsyncioBlockingUHID)
+    await device.wait_for_start_asyncio()
+    return device
+
+
+@pytest.fixture()
+@pytest.mark.asyncio
+def vendor_hidraw(vendor_device):
+    return find_hidraw(vendor_device)
+
+
+@pytest.mark.asyncio
+async def test_output(vendor_device, vendor_hidraw):
+    data = [0, 1, 2, 3, 4, 5, 6, 7]
+    received = []
+
+    def receive_output(data, report_type):
+        received.append(data)
+
+    vendor_device.receive_output = receive_output
+
+    os.write(vendor_hidraw.fd, bytes(data))
+    os.write(vendor_hidraw.fd, bytes(list(reversed(data))))
+
+    await asyncio.sleep(0.1)  # give some time for the events to be dispatched
+
+    assert received == [data, list(reversed(data))]
